1ba734416f340364db7ca7b15426de71d6ea29aa
Bassel Samo <basselsamo@hotmail.com>
2023-11-15 22:23:17 +0100

    setting up moodle for the first time

--- install-moodle.sh
+++ install-moodle.sh
@@ -3,7 +3,7 @@ set -x
 
 cp moodle-config.php /moodle/config.php
 
-docker exec -t --user www-data docker-moodle_moodleapp_1 php /var/www/html/admin/cli/install_database.php --agree-license --fullname="test-moodle" --shortname="test-moodle" --adminuser="admin" --adminpass="Admin12_" --adminemail="admin@localhost.de"
+docker exec -t --user www-data docker-moodle-moodleapp-1 php /var/www/html/admin/cli/install_database.php --agree-license --fullname="test-moodle" --shortname="test-moodle" --adminuser="admin" --adminpass="Admin12_" --adminemail="admin@localhost.de"
 
 # Enable analytics via GUI
-docker exec -t --user www-data docker-moodle_moodleapp_1 php /var/www/html/admin/cli/cfg.php --component=analytics --name=onlycli --set=0
+docker exec -t --user www-data docker-moodle-moodleapp-1 php /var/www/html/admin/cli/cfg.php --component=analytics --name=onlycli --set=0
