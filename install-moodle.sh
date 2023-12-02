2a3d7af270cbd37119c8b15c317dadbc7179b372
Natalia Marth <nataliamarth@gmail.com>
2023-12-02 19:25:02 +0100

    Fix install-moodle.sh on Linux. The moodle config file path should be relative and not absolute.

--- install-moodle.sh
+++ install-moodle.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
-cp moodle-config.php /moodle/config.php
+cp moodle-config.php ./moodle/config.php
 
 docker exec -t --user www-data docker-moodle-moodleapp-1 php /var/www/html/admin/cli/install_database.php --agree-license --fullname="test-moodle" --shortname="test-moodle" --adminuser="admin" --adminpass="Admin12_" --adminemail="admin@localhost.de"
 
