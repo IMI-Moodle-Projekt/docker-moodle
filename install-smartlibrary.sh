adac758ca0ea6594d900cafd58b2e9fccbd76ede
Natalia Marth <nataliamarth@gmail.com>
2023-12-02 19:52:07 +0100

    Fix install smartlibrary script. Correct folder name.

--- install-smartlibrary.sh
+++ install-smartlibrary.sh
@@ -2,6 +2,6 @@
 set -x
 
 mkdir moodle/local/smartlibrary
-cp -r "moodle-local_smartlibrary/"* "moodle/local/smartlibrary"
+cp -r "smartlibrary/"* "moodle/local/smartlibrary"
 docker exec -t --user www-data docker-moodle-moodleapp-1 php /var/www/html/admin/cli/upgrade.php --non-interactive
 
